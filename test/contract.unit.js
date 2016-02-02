809ef8828fe5f9a7d654c751bd4e062b6d7dd4ee
Gordon Hall <gordon@gordonwritescode.com>
2016-02-02 16:28:17 -0500

    add some basic tests to contract interface

--- test/contract.unit.js
+++ test/contract.unit.js
@@ -1,23 +1,72 @@
 'use strict';
 
+var expect = require('chai').expect;
 var Contract = require('../lib/contract');
 
 describe('Contract', function() {
 
-  describe('#fromObject');
+  describe('#fromObject', function() {
 
-  describe('#fromJSON');
+    it('should return an instance from the object', function() {
+      expect(Contract.fromObject({})).to.be.instanceOf(Contract);
+    });
 
-  describe('#fromBuffer');
+  });
 
-  describe('#validate');
+  describe('#fromJSON', function() {
 
-  describe('@constructor');
+    it('should return an instance from the json string', function() {
+      expect(Contract.fromJSON('{}')).to.be.instanceOf(Contract);
+    });
 
-  describe('#toObject');
+  });
 
-  describe('#toJSON');
+  describe('#fromBuffer', function() {
 
-  describe('#toBuffer');
+    it('should return an instance from the object', function() {
+      expect(Contract.fromBuffer(new Buffer('{}'))).to.be.instanceOf(Contract);
+    });
+
+  });
+
+  describe('#validate', function() {
+
+    it('should validate the contract specification', function() {
+      expect(function() {
+        Contract({ version: 1 });
+      }).to.not.throw(Error);
+    });
+
+    it('should invalidate the contract specification', function() {
+      expect(function() {
+        Contract({ version: -1 });
+      }).to.not.throw(Error);
+    });
+
+  });
+
+  describe('#toObject', function() {
+
+    it('should return an object representation of the contract', function() {
+      expect(typeof Contract().toObject()).to.equal('object');
+    });
+
+  });
+
+  describe('#toJSON', function() {
+
+    it('should return a JSON representation of the contract', function() {
+      expect(typeof Contract().toJSON()).to.equal('string');
+    });
+
+  });
+
+  describe('#toBuffer', function() {
+
+    it('should return a buffer representation of the contract', function() {
+      expect(Buffer.isBuffer(Contract().toBuffer())).to.equal(true);
+    });
+
+  });
 
 });

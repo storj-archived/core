edc2c2324406032539edccc9620b0045d6892896
Gordon Hall <gordon@gordonwritescode.com>
2016-02-02 15:47:46 -0500

    add first pass draft of contract spec

---
+++ test/contract.unit.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var Contract = require('../lib/contract');
+
+describe('Contract', function() {
+
+  describe('#fromObject');
+
+  describe('#fromJSON');
+
+  describe('#fromBuffer');
+
+  describe('#validate');
+
+  describe('@constructor');
+
+  describe('#toObject');
+
+  describe('#toJSON');
+
+  describe('#toBuffer');
+
+});

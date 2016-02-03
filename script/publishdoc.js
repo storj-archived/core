c9dbd6e29c9a4c5dcf830e0f78fca10f7fe18650
Gordon Hall <gordon@gordonwritescode.com>
2016-02-03 16:49:49 -0500

    update publishdoc

--- script/publishdoc.js
+++ script/publishdoc.js
@@ -3,6 +3,8 @@
 var ghpages = require('gh-pages');
 var path = require('path');
 
-ghpages.publish(path.join(__dirname, 'doc'), function(err) {
-  console.log(err || 'Documentation published!');
+ghpages.publish(path.join(__dirname, '../doc'), {
+  repo: 'git@github.com:Storj/storjnode-js.git'
+}, function(err) {
+  console.log(err || 'Published to http://storj.github.io/storjnode-js');
 });

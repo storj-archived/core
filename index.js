202f33ddd21a7907ce03653da00f6b14cc48a029
Gordon Hall <gordon@gordonwritescode.com>
2016-02-05 19:08:13 -0500

    implemented audits, proofs, and broken verifications

--- index.js
+++ index.js
@@ -12,8 +12,14 @@ module.exports.Network = require('./lib/network');
 /** {@link Contract} */
 module.exports.Contract = require('./lib/contract');
 
+/** {@link Audit} */
+module.exports.Audit = require('./lib/audit');
+
 /** {@link KeyPair} */
 module.exports.KeyPair = require('./lib/keypair');
 
 /** {@link constants} */
 module.exports.constants = require('./lib/constants');
+
+/** {@link utils} */
+module.exports.utils = require('./lib/utils');

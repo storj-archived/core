89c8fb8561a85d8651188cfebff6ae0f8deb0156
Gordon Hall <gordon@gordonwritescode.com>
2016-05-02 11:58:08 -0400

    add negotiator function to FarmerInterface for determining if farmer should send renter an offer

---
+++ test/interfaces/farmer.unit.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var expect = require('chai').expect;
+var Contract = require('../../lib/contract');
+var KeyPair = require('../../lib/keypair');
+var FarmerInterface = require('../../lib/interfaces/farmer');
+
+describe('FarmerInterface', function() {
+
+  describe('#_negotiateContract', function() {
+
+    it('should not send an offer if the negotiator returns false', function() {
+      var farmer = new FarmerInterface({
+        keypair: KeyPair(),
+        port: 0,
+        noforward: true,
+        negotiator: function() {
+          return false;
+        }
+      });
+      expect(farmer._negotiateContract(Contract({}))).to.equal(false);
+    });
+
+  });
+
+});

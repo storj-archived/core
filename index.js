8c58fec8cb37a43ec60258611202d19145e06852
Gordon Hall <gordon@gordonwritescode.com>
2016-05-23 16:58:11 -0400

    implement rate limiter and integrate into network interface

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ module.exports.Transport = require('./lib/network/transport');
 /** {@link Contact} */
 module.exports.Contact = require('./lib/network/contact');
 
+/** {@link RateLimiter} */
+module.exports.RateLimiter = require('./lib/network/ratelimiter');
+
 /** {@link DataChannelClient} */
 module.exports.DataChannelClient = require('./lib/datachannel/client');
 

7711874910e575d11d4829a49c50380cf77c9735
Gordon Hall <gordon@gordonwritescode.com>
2016-05-26 11:25:20 -0400

    increase test coverage

--- test/interfaces/farmer.unit.js
+++ test/interfaces/farmer.unit.js
@@ -5,9 +5,11 @@ var expect = require('chai').expect;
 var Contract = require('../../lib/contract');
 var KeyPair = require('../../lib/keypair');
 var FarmerInterface = require('../../lib/interfaces/farmer');
+var Network = require('../../lib/network');
 var kad = require('kad');
 var Contact = require('../../lib/network/contact');
 var utils = require('../../lib/utils');
+var StorageItem = require('../../lib/storage/item');
 
 describe('FarmerInterface', function() {
 
@@ -88,6 +90,273 @@ describe('FarmerInterface', function() {
         done();
       };
       farmer._negotiateContract(contract);
+    });
+
+    it('should remove contract from pending if save fails', function(done) {
+      var farmer = new FarmerInterface({
+        keypair: KeyPair(),
+        port: 0,
+        noforward: true,
+        logger: kad.Logger(0),
+        backend: require('memdown'),
+        storage: { path: 'test' }
+      });
+      var _remove = sinon.stub(farmer, '_removeContractFromPendingList');
+      var _getContactByNodeID = sinon.stub(
+        farmer._router,
+        'getContactByNodeID'
+      ).returns(null);
+      var _save = sinon.stub(farmer._manager, 'save').callsArgWith(
+        1,
+        new Error('Save failed')
+      );
+      farmer._negotiateContract(Contract({
+        data_hash: utils.rmd160(' some data')
+      }));
+      setImmediate(function() {
+        _getContactByNodeID.restore();
+        _save.restore();
+        _remove.restore();
+        expect(_remove.called).to.equal(true);
+        done();
+      });
+    });
+
+    it('should remove contract from pending if lookup fails', function(done) {
+      var farmer = new FarmerInterface({
+        keypair: KeyPair(),
+        port: 0,
+        noforward: true,
+        logger: kad.Logger(0),
+        backend: require('memdown'),
+        storage: { path: 'test' }
+      });
+      var _save = sinon.stub(farmer._manager, 'save').callsArgWith(
+        1,
+        null
+      );
+      var _remove = sinon.stub(farmer, '_removeContractFromPendingList');
+      var _getContactByNodeID = sinon.stub(
+        farmer._router,
+        'getContactByNodeID'
+      ).returns(null);
+      var _findNode = sinon.stub(farmer._router, 'findNode').callsArgWith(
+        1,
+        new Error('Lookup failed')
+      );
+      farmer._negotiateContract(Contract({
+        data_hash: utils.rmd160('some data')
+      }));
+      setImmediate(function() {
+        setImmediate(function() {
+          _save.restore();
+          _getContactByNodeID.restore();
+          _remove.restore();
+          _findNode.restore();
+          expect(_remove.called).to.equal(true);
+          done();
+        });
+      });
+    });
+
+    it('should remove contract from pending if no renter', function(done) {
+      var farmer = new FarmerInterface({
+        keypair: KeyPair(),
+        port: 0,
+        noforward: true,
+        logger: kad.Logger(0),
+        backend: require('memdown'),
+        storage: { path: 'test' }
+      });
+      var _remove = sinon.stub(farmer, '_removeContractFromPendingList');
+      var _getContactByNodeID = sinon.stub(
+        farmer._router,
+        'getContactByNodeID'
+      ).returns(null);
+      var _findNode = sinon.stub(farmer._router, 'findNode').callsArgWith(
+        1,
+        null,
+        []
+      );
+      farmer._negotiateContract(Contract({
+        data_hash: utils.rmd160('some data')
+      }));
+      setImmediate(function() {
+        setImmediate(function() {
+          setImmediate(function() {
+            _getContactByNodeID.restore();
+            _findNode.restore();
+            _remove.restore();
+            expect(_remove.called).to.equal(true);
+            done();
+          });
+        });
+      });
+    });
+
+  });
+
+  describe('#join', function() {
+
+    it('should bubble error from Network#join', function(done) {
+      var farmer = new FarmerInterface({
+        keypair: KeyPair(),
+        port: 0,
+        noforward: true,
+        logger: kad.Logger(0),
+        backend: require('memdown'),
+        storage: { path: 'test' }
+      });
+      var _join = sinon.stub(Network.prototype, 'join').callsArgWith(
+        0,
+        new Error('Failed to join network')
+      );
+      farmer.join(function(err) {
+        _join.restore();
+        expect(err.message).to.equal('Failed to join network');
+        done();
+      });
+    });
+
+  });
+
+  describe('#_sendOfferForContract', function() {
+
+    it('should log an error if transport send fails', function(done) {
+      var farmer = new FarmerInterface({
+        keypair: KeyPair(),
+        port: 0,
+        noforward: true,
+        logger: kad.Logger(0),
+        backend: require('memdown'),
+        storage: { path: 'test' }
+      });
+      var _send = sinon.stub(farmer._transport, 'send').callsArgWith(
+        2,
+        new Error('Failed to send offer')
+      );
+      var _error = sinon.stub(farmer._logger, 'error');
+      farmer._sendOfferForContract({
+        toObject: sinon.stub(),
+        get: sinon.stub()
+      });
+      setImmediate(function() {
+        _send.restore();
+        _error.restore();
+        expect(_error.calledWith('Failed to send offer')).to.equal(true);
+        done();
+      });
+    });
+
+    it('should log default error if none provided', function(done) {
+      var farmer = new FarmerInterface({
+        keypair: KeyPair(),
+        port: 0,
+        noforward: true,
+        logger: kad.Logger(0),
+        backend: require('memdown'),
+        storage: { path: 'test' }
+      });
+      var _send = sinon.stub(farmer._transport, 'send').callsArgWith(
+        2,
+        null,
+        { result: {} }
+      );
+      var _error = sinon.stub(farmer._logger, 'error');
+      farmer._sendOfferForContract({
+        toObject: sinon.stub(),
+        get: sinon.stub()
+      });
+      setImmediate(function() {
+        _send.restore();
+        _error.restore();
+        expect(_error.calledWith('Renter refused to sign')).to.equal(true);
+        done();
+      });
+    });
+
+  });
+
+  describe('#_handleContractPublication', function() {
+
+    it('should return false for invalid contract', function() {
+      expect(
+        FarmerInterface.prototype._handleContractPublication({ version: '12' })
+      ).to.equal(false);
+    });
+
+  });
+
+  describe('#_handleOfferRes', function() {
+
+    it('should stop and log error if invalid contract', function(done) {
+      var farmer = new FarmerInterface({
+        keypair: KeyPair(),
+        port: 0,
+        noforward: true,
+        logger: kad.Logger(0),
+        backend: require('memdown'),
+        storage: { path: 'test' }
+      });
+      var _error = sinon.stub(farmer._logger, 'error');
+      farmer._handleOfferRes({ result: { contract: { version: '12'} } });
+      setImmediate(function() {
+        _error.restore();
+        expect(
+          _error.calledWith('Renter responded with invalid contract')
+        ).to.equal(true);
+        done();
+      });
+    });
+
+    it('should stop and log error if signature invalid', function(done) {
+      var farmer = new FarmerInterface({
+        keypair: KeyPair(),
+        port: 0,
+        noforward: true,
+        logger: kad.Logger(0),
+        backend: require('memdown'),
+        storage: { path: 'test' }
+      });
+      var _error = sinon.stub(farmer._logger, 'error');
+      farmer._handleOfferRes({
+        result: {
+          contract: Contract({}).toObject()
+        }
+      }, new Contract());
+      setImmediate(function() {
+        _error.restore();
+        expect(
+          _error.calledWith('Renter signature is invalid')
+        ).to.equal(true);
+        done();
+      });
+    });
+
+    it('should create a new item if cannot load existing', function(done) {
+      var farmer = new FarmerInterface({
+        keypair: KeyPair(),
+        port: 0,
+        noforward: true,
+        logger: kad.Logger(0),
+        backend: require('memdown'),
+        storage: { path: 'test' }
+      });
+      var _load = sinon.stub(farmer._manager, 'load').callsArgWith(1, {});
+      var _save = sinon.stub(farmer._manager, 'save');
+      var _verify = sinon.stub(Contract.prototype, 'verify').returns(true);
+      farmer._handleOfferRes({
+        result: {
+          contract: Contract({}).toObject()
+        }
+      }, new Contract(), {});
+      setImmediate(function() {
+        _load.restore();
+        _save.restore();
+        _verify.restore();
+        expect(_save.args[0][0]).to.be.instanceOf(StorageItem);
+        done();
+      });
     });
 
   });

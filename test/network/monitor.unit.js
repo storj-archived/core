54b917d65da75d7a2468a40f9aa0e43b962edb8a
Gordon Hall <gordon@gordonwritescode.com>
2016-07-14 16:33:30 -0400

    add monitor class

---
+++ test/network/monitor.unit.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Monitor = require('../../lib/network/monitor');
+var expect = require('chai').expect;
+var sinon = require('sinon');
+
+describe('Network/Monitor', function() {
+
+
+
+});

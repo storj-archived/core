5312f6407bf2b6b6ec1a9552ad179b00e0c37554
aleitner <leitnersalex@gmail.com>
2016-08-10 16:11:17 -0400

    Updating examples

---
+++ example/5b-addBucket.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var storj = require('storj');
+var fs = require('fs');
+// Set the bridge api URL
+var api = 'https://api.storj.io';
+
+// Load keypair from your saved private key
+var keypair = storj.KeyPair(fs.readFileSync('./private.key').toString());
+
+// Login using the keypair generated
+var client = storj.BridgeClient(api, {keypair: keypair});
+
+var bucketInfo = {
+  name: 'Cool bucket',
+  storage: 30,
+  transfer: 10
+};
+
+client.createBucket(bucketInfo, function(err, bucket) {
+  if (err) {
+    return console.log('error', err.message);
+  }
+
+  console.log(
+    'info',
+    'ID: %s, Name: %s, Storage: %s, Transfer: %s',
+    [bucket.id, bucket.name, bucket.storage, bucket.transfer]
+  );
+});

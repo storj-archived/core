441102870f4a1dbe4cb967852b30a45f244affc9
Gordon Hall <gordon@gordonwritescode.com>
2016-08-25 15:36:40 -0400

    handle errors in more places and add some more debug and info logging

--- test/tunnel/gateway.unit.js
+++ test/tunnel/gateway.unit.js
@@ -118,6 +118,24 @@ describe('TunnelGateway', function() {
       req.emit('end');
     });
 
+    it('should log an error from the incoming message', function(done) {
+      var gw = TunnelGateway();
+      gw._logger.error = sinon.stub();
+      var req = new events.EventEmitter();
+      var res = {
+        writeHead: function(code) {
+          expect(code).to.equal(400);
+        },
+        end: done
+      };
+      gw._handleRPC(req, res);
+      setImmediate(function() {
+        req.emit('error', new Error('socket hang up'));
+        expect(gw._logger.error.called).to.equal(true);
+        done();
+      });
+    });
+
   });
 
   describe('#open', function() {

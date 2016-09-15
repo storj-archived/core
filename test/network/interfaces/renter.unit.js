8a88d6009fba73fd1d173cabb30f757c90dfd096
Gordon Hall <gordon@gordonwritescode.com>
2016-09-15 18:57:14 -0400

    coverage on renter interface

--- test/network/interfaces/renter.unit.js
+++ test/network/interfaces/renter.unit.js
@@ -14,7 +14,26 @@ var StorageManager = require('../../../lib/storage/manager');
 var AuditStream = require('../../../lib/audit-tools/audit-stream');
 var DataChannelPointer = require('../../../lib/data-channels/pointer');
 
+var CLEANUP = [];
+
 describe('RenterInterface', function() {
+
+  describe('@constructor', function() {
+
+    it('should create an instance without the new keyword', function() {
+      var renter = RenterInterface({
+        keyPair: KeyPair(),
+        rpcPort: 0,
+        doNotTraverseNat: true,
+        logger: kad.Logger(0),
+        tunnelServerPort: 0,
+        storageManager: StorageManager(RAMStorageAdapter())
+      });
+      CLEANUP.push(renter);
+      expect(renter).to.be.instanceOf(RenterInterface);
+    });
+
+  });
 
   describe('#getStorageOffer', function() {
 
@@ -27,6 +46,7 @@ describe('RenterInterface', function() {
         tunnelServerPort: 0,
         storageManager: StorageManager(RAMStorageAdapter())
       });
+      CLEANUP.push(renter);
       var clock = sinon.useFakeTimers();
       var contract = Contract({});
       var publish = sinon.stub(renter, 'publish');
@@ -41,6 +61,25 @@ describe('RenterInterface', function() {
       clock.tick(15000);
     });
 
+    it('should default the blacklist to empty', function() {
+      var renter = new RenterInterface({
+        keyPair: KeyPair(),
+        rpcPort: 0,
+        tunnelServerPort: 0,
+        doNotTraverseNat: true,
+        logger: kad.Logger(0),
+        storageManager: StorageManager(RAMStorageAdapter())
+      });
+      CLEANUP.push(renter);
+      var contract = Contract({});
+      var publish = sinon.stub(renter, 'publish');
+      var callback = sinon.stub();
+      renter.getStorageOffer(contract, callback);
+      expect(renter._pendingContracts[
+        Object.keys(renter._pendingContracts)[0]
+      ].blacklist).to.have.lengthOf(0);
+    });
+
     it('should do nothing if the callback was already called', function(done) {
       var renter = new RenterInterface({
         keyPair: KeyPair(),
@@ -50,6 +89,7 @@ describe('RenterInterface', function() {
         logger: kad.Logger(0),
         storageManager: StorageManager(RAMStorageAdapter())
       });
+      CLEANUP.push(renter);
       var clock = sinon.useFakeTimers();
       var contract = Contract({});
       var publish = sinon.stub(renter, 'publish');
@@ -78,6 +118,7 @@ describe('RenterInterface', function() {
         logger: kad.Logger(0),
         storageManager: StorageManager(RAMStorageAdapter())
       });
+      CLEANUP.push(renter);
       var farmer = new Contact({
         address: '127.0.0.1',
         port: 6666,
@@ -101,6 +142,7 @@ describe('RenterInterface', function() {
         logger: kad.Logger(0),
         storageManager: StorageManager(RAMStorageAdapter())
       });
+      CLEANUP.push(renter);
       var nodeID = KeyPair().getNodeID();
       var farmer = new Contact({
         address: '127.0.0.1',
@@ -138,6 +180,7 @@ describe('RenterInterface', function() {
         logger: kad.Logger(0),
         storageManager: StorageManager(RAMStorageAdapter())
       });
+      CLEANUP.push(renter);
       var nodeID = KeyPair().getNodeID();
       var farmer = new Contact({
         address: '127.0.0.1',
@@ -177,6 +220,7 @@ describe('RenterInterface', function() {
         logger: kad.Logger(0),
         storageManager: StorageManager(RAMStorageAdapter())
       });
+      CLEANUP.push(renter);
       var nodeID = KeyPair().getNodeID();
       var farmer = new Contact({
         address: '127.0.0.1',
@@ -216,6 +260,7 @@ describe('RenterInterface', function() {
         logger: kad.Logger(0),
         storageManager: StorageManager(RAMStorageAdapter())
       });
+      CLEANUP.push(renter);
       var nodeID = KeyPair().getNodeID();
       var farmer = new Contact({
         address: '127.0.0.1',
@@ -246,6 +291,46 @@ describe('RenterInterface', function() {
       });
     });
 
+    it('should succeed if farmer responds with valid proof', function(done) {
+      var renter = new RenterInterface({
+        keyPair: KeyPair(),
+        rpcPort: 0,
+        tunnelServerPort: 0,
+        doNotTraverseNat: true,
+        logger: kad.Logger(0),
+        storageManager: StorageManager(RAMStorageAdapter())
+      });
+      CLEANUP.push(renter);
+      var nodeID = KeyPair().getNodeID();
+      var farmer = new Contact({
+        address: '127.0.0.1',
+        port: 6666,
+        nodeID: nodeID
+      });
+      var data = {
+        contracts: {},
+        challenges: {}
+      };
+      data.contracts[nodeID] = new Contract({
+        renter_id: renter.keyPair.getNodeID(),
+        data_size: 10,
+        data_hash: 'a8a412aaf3cc8da088ef00d9d6185fe94fc9f9bc',
+        store_begin: Date.now(),
+        store_end: Date.now() + 10000,
+        audit_count: 12
+      });
+      data.challenges[nodeID] = { challenges: ['challenge'] };
+      var item = new StorageItem(data);
+      var _send = sinon.stub(renter.transport, 'send').callsArgWith(
+        2, null, { result: { proofs: ['VALID?'] } }
+      );
+      renter.getStorageProof(farmer, item, function(err, proof) {
+        expect(proof).to.equal('VALID?');
+        _send.restore();
+        done();
+      });
+    });
+
   });
 
   describe('#getConsignToken', function() {
@@ -259,6 +344,7 @@ describe('RenterInterface', function() {
         logger: kad.Logger(0),
         storageManager: StorageManager(RAMStorageAdapter())
       });
+      CLEANUP.push(renter);
       var _send = sinon.stub(renter.transport, 'send').callsArgWith(
         2,
         new Error('Send failed')
@@ -278,7 +364,7 @@ describe('RenterInterface', function() {
       });
     });
 
-    it('should callback error if contract responds with one', function(done) {
+    it('should callback error if contact responds with one', function(done) {
       var renter = new RenterInterface({
         keyPair: KeyPair(),
         rpcPort: 0,
@@ -287,6 +373,7 @@ describe('RenterInterface', function() {
         logger: kad.Logger(0),
         storageManager: StorageManager(RAMStorageAdapter())
       });
+      CLEANUP.push(renter);
       var _send = sinon.stub(renter.transport, 'send').callsArgWith(
         2,
         null,
@@ -307,6 +394,38 @@ describe('RenterInterface', function() {
       });
     });
 
+    it('should callback error if contact responds with one', function(done) {
+      var renter = new RenterInterface({
+        keyPair: KeyPair(),
+        rpcPort: 0,
+        tunnelServerPort: 0,
+        doNotTraverseNat: true,
+        logger: kad.Logger(0),
+        storageManager: StorageManager(RAMStorageAdapter())
+      });
+      CLEANUP.push(renter);
+      var _send = sinon.stub(renter.transport, 'send').callsArgWith(
+        2,
+        null,
+        { result: { token: utils.rmd160('') } }
+      );
+      var audit = new AuditStream(1);
+      audit.end(Buffer('data'));
+      setImmediate(function() {
+        renter.getConsignToken(Contact({
+          address: '0.0.0.0',
+          port: 0,
+          nodeID: utils.rmd160('contact')
+        }), Contract({
+          data_hash: utils.rmd160('')
+        }), audit, function(err, token) {
+          _send.restore();
+          expect(token).to.equal(utils.rmd160(''));
+          done();
+        });
+      });
+    });
+
   });
 
   describe('#getRetrieveToken', function() {
@@ -320,6 +439,7 @@ describe('RenterInterface', function() {
         logger: kad.Logger(0),
         storageManager: StorageManager(RAMStorageAdapter())
       });
+      CLEANUP.push(renter);
       var _send = sinon.stub(renter.transport, 'send').callsArgWith(
         2,
         new Error('Send failed')
@@ -344,6 +464,7 @@ describe('RenterInterface', function() {
         logger: kad.Logger(0),
         storageManager: StorageManager(RAMStorageAdapter())
       });
+      CLEANUP.push(renter);
       var _send = sinon.stub(renter.transport, 'send').callsArgWith(
         2,
         null,
@@ -360,6 +481,35 @@ describe('RenterInterface', function() {
       });
     });
 
+    it('should callback error if contact responds with one', function(done) {
+      var renter = new RenterInterface({
+        keyPair: KeyPair(),
+        rpcPort: 0,
+        tunnelServerPort: 0,
+        doNotTraverseNat: true,
+        logger: kad.Logger(0),
+        storageManager: StorageManager(RAMStorageAdapter())
+      });
+      CLEANUP.push(renter);
+      var _send = sinon.stub(renter.transport, 'send').callsArgWith(
+        2,
+        null,
+        { result: { token: utils.rmd160('') } }
+      );
+      setImmediate(function() {
+        renter.getRetrieveToken(Contact({
+          address: '0.0.0.0',
+          port: 0,
+          nodeID: utils.rmd160('contact')
+        }), Contract({
+          data_hash: utils.rmd160('')
+        }), function(err, token) {
+          _send.restore();
+          expect(token).to.equal(utils.rmd160(''));
+          done();
+        });
+      });
+    });
   });
 
   describe('#getMirrorNodes', function() {
@@ -373,6 +523,7 @@ describe('RenterInterface', function() {
         logger: kad.Logger(0),
         storageManager: StorageManager(RAMStorageAdapter())
       });
+      CLEANUP.push(renter);
       var _send = sinon.stub(renter.transport, 'send').callsArgWith(
         2,
         new Error('Send failed')
@@ -396,6 +547,48 @@ describe('RenterInterface', function() {
       });
     });
 
+    it('should callback error if all nodes fail', function(done) {
+      var renter = new RenterInterface({
+        keyPair: KeyPair(),
+        rpcPort: 0,
+        tunnelServerPort: 0,
+        doNotTraverseNat: true,
+        logger: kad.Logger(0),
+        storageManager: StorageManager(RAMStorageAdapter())
+      });
+      CLEANUP.push(renter);
+      var _send = sinon.stub(renter.transport, 'send').callsArgWith(
+        2,
+        null,
+        { result: {} }
+      );
+      renter.getMirrorNodes([DataChannelPointer(
+        Contact({
+          address: '0.0.0.0',
+          port: 0,
+          nodeID: utils.rmd160('contact')
+        }),
+        utils.rmd160('hash'),
+        utils.generateToken()
+      )], [Contact({
+        address: '0.0.0.0',
+        port: 0,
+        nodeID: utils.rmd160('contact')
+      })], function(err, results) {
+        _send.restore();
+        expect(results).to.lengthOf(1);
+        done();
+      });
+    });
+
+  });
+
+  after(function() {
+    CLEANUP.forEach(function(renter) {
+      if (renter.node) {
+        renter.leave();
+      }
+    });
   });
 
 });

bdb2fbda396a2b89db3a9d28ab360ff04fef02af
Gordon Hall <gordon@gordonwritescode.com>
2016-09-23 12:04:20 -0400

    update nomenclature for new renter interface methods, change arg ordering in unhandled offer events, add tests

--- test/network/protocol.unit.js
+++ test/network/protocol.unit.js
@@ -59,7 +59,8 @@ describe('Protocol', function() {
           },
           _pendingContracts: {
             adc83b19e793491b1c6ea0fd8b46cd9f32e592fc: function() {}
-          }
+          },
+          acceptOffer: sinon.stub()
         }
       });
       var _verify = sinon.stub(proto, '_verifyContract').callsArg(2);
@@ -99,7 +100,7 @@ describe('Protocol', function() {
       }, function(err) {
         _verify.restore();
         expect(err).to.equal(null);
-        _network.on('unhandledOfferResolved', function(contract) {
+        _network.on('unhandledOfferResolved', function(contact, contract) {
           expect(contract.data_hash).to.equal(
             'adc83b19e793491b1c6ea0fd8b46cd9f32e592fc'
           );
@@ -107,6 +108,7 @@ describe('Protocol', function() {
         });
       });
     });
+
     it('should succeed and start consignment', function(done) {
       var _save = sinon.stub().callsArg(1);
       var _doConsign = sinon.stub();
@@ -118,6 +120,9 @@ describe('Protocol', function() {
           },
           _pendingContracts: {
             adc83b19e793491b1c6ea0fd8b46cd9f32e592fc: _doConsign
+          },
+          acceptOffer: function() {
+            _doConsign();
           }
         }
       });

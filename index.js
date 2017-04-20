a0f0b3a862a2c05b91c5cacb6e306422a5b50c8f
Gordon Hall <gordon@gordonwritescode.com>
2017-04-20 17:19:52 -0400

    update contract, remove file handling code, crypto tools

--- index.js
+++ index.js
@@ -1,8 +1,50 @@
 /**
  * @module storj
- * @license (AGPL-3.0 AND LGPL-3.0)
+ * @license AGPL-3.0
  */
 
 'use strict';
 
-module.exports = {};
+/**
+ * Returns a new {@link StorjNode}
+ * @function
+ */
+module.exports = function(options) {
+  return module.exports.Node(options);
+};
+
+/** {@link Node} */
+module.exports.Node = require('./lib/node');
+
+/** {@link Rules} */
+module.exports.Rules = require('./lib/rules');
+
+/** {@link Transport} */
+module.exports.Transport = require('./lib/transport');
+
+/** {@link Server} */
+module.exports.Server = require('./lib/server');
+
+/** {@link Audit} */
+module.exports.Audit = require('./lib/audit');
+
+/** {@link Proof} */
+module.exports.Proof = require('./lib/proof');
+
+/** {@link Offers} */
+module.exports.Offers = require('./lib/offers');
+
+/** {@link Triggers} */
+module.exports.Triggers = require('./lib/triggers');
+
+/** {@link Contract} */
+module.exports.Contract = require('./lib/contract');
+
+/** {@link module:constants} */
+module.exports.constants = require('./lib/constants');
+
+/** {@link module:utils} */
+module.exports.utils = require('./lib/utils');
+
+/** {@link module:version} */
+module.exports.version = require('./lib/version');

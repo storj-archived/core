7c9c724154c71b7d08e3dbf80c7db1ff52295232
Gordon Hall <gordon@gordonwritescode.com>
2017-05-08 14:26:25 -0400

    add audit rules coverage

--- test/rules.unit.js
+++ test/rules.unit.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const { expect } = require('chai');
+const sinon = require('sinon');
 const crypto = require('crypto');
 const { utils: keyutils } = require('kad-spartacus');
+const { Readable: ReadableStream } = require('stream');
 const OfferStream = require('../lib/offers');
+const AuditStream = require('../lib/audit');
+const ProofStream = require('../lib/proof');
 const Contract = require('../lib/contract');
 const Rules = require('../lib/rules');
 
@@ -109,7 +113,169 @@ describe('@class Rules', function() {
 
   describe('@method audit', function() {
 
+    let auditStream = null;
+    let dataShard = Buffer.from('this is a test shard');
 
+    before(function(done) {
+      auditStream = new AuditStream(2);
+      auditStream.on('finish', done).end(dataShard);
+    });
+
+    it('should callback error if invalid audit batch', function(done) {
+      const rules = new Rules({});
+      const request = {
+        params: {},
+        contact: [
+          'identity',
+          { xpub: 'xpubkey' }
+        ]
+      };
+      const response = {};
+      rules.audit(request, response, (err) => {
+        expect(err.message).to.equal('Invalid audit batch supplied');
+        done();
+      });
+    });
+
+    it('should return null if cannot load contract', function(done) {
+      const rules = new Rules({
+        contracts: {
+          get: sinon.stub().callsArgWith(1, new Error('Not found'))
+        }
+      });
+      const request = {
+        params: [
+          { hash: 'datahash', challenge: 'challengerequest' }
+        ],
+        contact: [
+          'identity',
+          { xpub: 'xpubkey' }
+        ]
+      };
+      const response = {
+        send: (params) => {
+          expect(params[0].hash).to.equal('datahash');
+          expect(params[0].proof).to.equal(null);
+          done();
+        }
+      };
+      rules.audit(request, response, done);
+    });
+
+    it('should return null if cannot load shard', function(done) {
+      const rules = new Rules({
+        contracts: {
+          get: sinon.stub().callsArgWith(1, null, { data_hash: 'datahash'})
+        },
+        shards: {
+          createReadStream: sinon.stub().callsArgWith(
+            1,
+            new Error('Not found')
+          )
+        }
+      });
+      const request = {
+        params: [
+          { hash: 'datahash', challenge: 'challengerequest' }
+        ],
+        contact: [
+          'identity',
+          { xpub: 'xpubkey' }
+        ]
+      };
+      const response = {
+        send: (params) => {
+          expect(params[0].hash).to.equal('datahash');
+          expect(params[0].proof).to.equal(null);
+          done();
+        }
+      };
+      rules.audit(request, response, done);
+    });
+
+    it('should return null if proof fails', function(done) {
+      const shardParts = [dataShard];
+      const readStream = new ReadableStream({
+        read: function() {
+          if (shardParts.length) {
+            this.push(shardParts.shift());
+          } else {
+            this.push(null);
+          }
+        }
+      });
+      const rules = new Rules({
+        contracts: {
+          get: sinon.stub().callsArgWith(1, null, {
+            data_hash: 'datahash',
+            audit_leaves: auditStream.getPublicRecord()
+          })
+        },
+        shards: {
+          createReadStream: sinon.stub().callsArgWith(1, null, readStream)
+        }
+      });
+      const request = {
+        params: [{ hash: 'datahash', challenge: '00000000' }],
+        contact: [
+          'identity',
+          { xpub: 'xpubkey' }
+        ]
+      };
+      const response = {
+        send: (params) => {
+          expect(params[0].proof).to.equal(null);
+          done();
+        }
+      };
+      rules.audit(request, response);
+    });
+
+    it('should return { hash, proof } if successful', function(done) {
+      const shardParts = [dataShard];
+      const readStream = new ReadableStream({
+        read: function() {
+          if (shardParts.length) {
+            this.push(shardParts.shift());
+          } else {
+            this.push(null);
+          }
+        }
+      });
+      const rules = new Rules({
+        contracts: {
+          get: sinon.stub().callsArgWith(1, null, {
+            data_hash: 'datahash',
+            audit_leaves: auditStream.getPublicRecord()
+          })
+        },
+        shards: {
+          createReadStream: sinon.stub().callsArgWith(1, null, readStream)
+        }
+      });
+      const request = {
+        params: [
+          {
+            hash: 'datahash',
+            challenge: auditStream.getPrivateRecord().challenges[0]
+          }
+        ],
+        contact: [
+          'identity',
+          { xpub: 'xpubkey' }
+        ]
+      };
+      const response = {
+        send: (params) => {
+          let { proof } = params[0];
+          let { root, depth } = auditStream.getPrivateRecord();
+          let [expected, actual] = ProofStream.verify(proof, root, depth);
+          expect(Buffer.compare(expected, actual)).to.equal(0);
+          done();
+        }
+      };
+      rules.audit(request, response);
+    });
 
   });
 

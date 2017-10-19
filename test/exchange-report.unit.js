9c3fb0a69007da5f992f2af2e06b64f6720a05b7
Braydon Fuller <courier@braydon.com>
2017-10-19 14:43:16 -0400

    Update exchange reports

--- test/exchange-report.unit.js
+++ test/exchange-report.unit.js
@@ -13,9 +13,7 @@ describe('Exchange Report', function() {
   it('#toObject', function() {
     var report = new ExchangeReport({
       dataHash: 'd8e33c898eb5ccd6789b32ceaca4e1d4e8cc452f',
-      reporterId: '538db66de92c02dc52cde37307ace5463917cbf0',
-      farmerId: '41ff35b1fac9981685b4423f91acc11ec345cd36',
-      clientId: '2c33e90b668313c96733b269a3f5c7b7ab2128e1',
+      token: '5412f65e73825adb616649cfddc552e8e188edd4',
       exchangeStart: 1479247169809,
       exchangeEnd: 1479250511087,
       exchangeResultCode: 1000,
@@ -24,9 +22,7 @@ describe('Exchange Report', function() {
 
     expect(report.toObject()).to.deep.equal({
       dataHash: 'd8e33c898eb5ccd6789b32ceaca4e1d4e8cc452f',
-      reporterId: '538db66de92c02dc52cde37307ace5463917cbf0',
-      farmerId: '41ff35b1fac9981685b4423f91acc11ec345cd36',
-      clientId: '2c33e90b668313c96733b269a3f5c7b7ab2128e1',
+      token: '5412f65e73825adb616649cfddc552e8e188edd4',
       exchangeStart: 1479247169809,
       exchangeEnd: 1479250511087,
       exchangeResultCode: 1000,
@@ -38,9 +34,7 @@ describe('Exchange Report', function() {
   describe('#begin/#end', function() {
     it('it should record start and end time', function() {
       var report = new ExchangeReport({
-        reporterId: '538db66de92c02dc52cde37307ace5463917cbf0',
-        farmerId: '41ff35b1fac9981685b4423f91acc11ec345cd36',
-        clientId: '2c33e90b668313c96733b269a3f5c7b7ab2128e1',
+        token: '89e1625e692c4546d6cda29231f6890cbb8c31b7'
       });
 
       expect(report._r.exchangeStart).to.equal(null);
@@ -57,9 +51,7 @@ describe('Exchange Report', function() {
       expect(report._r.exchangeEnd).to.equal(1479251141714);
 
       expect(report.toObject()).to.deep.equal({
-        reporterId: '538db66de92c02dc52cde37307ace5463917cbf0',
-        farmerId: '41ff35b1fac9981685b4423f91acc11ec345cd36',
-        clientId: '2c33e90b668313c96733b269a3f5c7b7ab2128e1',
+        token: '89e1625e692c4546d6cda29231f6890cbb8c31b7',
         dataHash: '55736f88e72c42e732defdbfda258cbe0f8be94b',
         exchangeStart: 1479251099379,
         exchangeEnd: 1479251141714,
